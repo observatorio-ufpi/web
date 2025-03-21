c6735344d833be9056dcec78b2a2bfec184bc7ab
RamonMatheus27 <matheusramon26@gmail.com>
2025-03-21 16:32:59 -0300

    fix: corrigindo importacao de component

--- src/components/pages/education/ApiComponent.js
+++ src/components/pages/education/ApiComponent.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { processResults as processApiResults } from "../services/dataProcessors";
+import { processResults as processApiResults } from "../../../services/dataProcessors";
 
 function ApiContainer({
   type,
